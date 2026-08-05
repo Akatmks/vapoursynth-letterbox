34ac989859b18e210203e68dc6cc8b3a29723462
Akatsumekusa <Akatsumekusa@protonmail.com>
2026-08-05 17:57:40 +0000

    Adjust

--- vsletterboxc/vsletterbox.cpp
+++ vsletterboxc/vsletterbox.cpp
@@ -111,8 +111,8 @@ static inline double calc_mean(const T * VS_RESTRICT srcp, int width) {
 //     return std::sqrt(sum / width) / max_value;
 // }
 
-template <typename T, double power = 1.5>
-static inline double calc_power_mean(const T * VS_RESTRICT srcp, int width) {
+template <typename T>
+static inline double calc_15_power_mean(const T * VS_RESTRICT srcp, int width) {
     constexpr T max_value = get_max_value<T>();
     constexpr T min_value = get_min_value<T>();
 
@@ -121,18 +121,20 @@ static inline double calc_power_mean(const T * VS_RESTRICT srcp, int width) {
         #pragma clang loop vectorize(enable) interleave(enable)
         for (int x = 0; x < width; x++) {
             #pragma clang fp reassociate(on)
-            sum += std::pow(static_cast<double>(srcp[x]), power);
+            const auto x_ = static_cast<double>(srcp[x]);
+            sum += x_ * std::sqrt(x_);
         }
     }
     else {
         #pragma clang loop vectorize(enable) interleave(enable)
         for (int x = 0; x < width; x++) {
             #pragma clang fp reassociate(on)
-            sum += std::pow(static_cast<double>(std::clamp(srcp[x], min_value, max_value)), power);
+            const auto x_ = static_cast<double>(std::clamp(srcp[x], min_value, max_value));
+            sum += x_ * std::sqrt(x_);
         }
     }
 
-    return std::pow(sum / width, 1 / power) / max_value;
+    return std::pow(sum / width, 2/3) / max_value;
 }
 
 // Incremental calculation of weighted mean and variance, Tony Finch, 2009
@@ -221,7 +223,7 @@ static const VSFrame * VS_CC letterbox_search_get_frame(int n, int activationRea
         int  bord_y = height;
         auto cutoff = false;
         for (; start_y < height; start_y++) {
-            const auto src_mean = calc_power_mean<T>(srcp, width);
+            const auto src_mean = calc_15_power_mean<T>(srcp, width);
 
             const auto st_mean = stats.mean();
             const auto st_stddev = stats.stddev();
@@ -255,7 +257,7 @@ static const VSFrame * VS_CC letterbox_search_get_frame(int n, int activationRea
             bord_y = -1;
             cutoff = false;
             for (; end_y >= 0; end_y--) {
-                const auto src_mean = calc_power_mean<T>(srcp, width);
+                const auto src_mean = calc_15_power_mean<T>(srcp, width);
 
                 const auto st_mean = stats.mean();
                 const auto st_stddev = stats.stddev();
